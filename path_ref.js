bb44b71fe25b68294e3212a36b2feb1e19dff50b
Sergei <sergei.smirnov001@ivkhk.ee>
2021-11-16 08:59:19 +0200

    2.Path

--- path_ref.js
+++ path_ref.js
@@ -1,8 +1,11 @@
 const path = require('path')
 
 constol.log(__filename)
+// The default operation of the path module varies based on the operating system on which a Node.js application is running.
 console.log(path.basename(__filename))
+// The path.dirname() method returns the directory name of a path, similar to the Unix dirname command. 
 console.log(path.dirname(__filename))
+// he path.extname() method returns the extension of the path, from the last occurrence of the . (period) character to end of string in the last portion of the path. If there is no .
 console.log(path.extname(__filename))
 
 console.log(path.parse(__filename))
